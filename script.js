0894bcfca9b1cbc49666e8171fc0d28e9b86e17e
Quent740 <Quentindg740@gmail.com>
2024-11-27 16:23:37 +0100

    edit modification

--- script.js
+++ script.js
@@ -1,16 +1,57 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const todoList = document.getElementById('todo-list');
 
-    document.getElementById('new-todo-item-add').addEventListener('click', function() {
-        const newItemText = document.getElementById('new-todo-item-title').value.trim();  // Récupère le texte de l'input et le nettoie des espaces inutiles
+    //Bouton
+    const addButton = document.getElementById('new-todo-item-add');
+    const confirmEditButton = document.getElementById('edit-todo-item-confirm');
+    const cancelEditButton = document.getElementById('edit-todo-item-cancel');
+
+    //Label
+    const inputField = document.getElementById('new-todo-item-title');
+    const inputEdit = document.getElementById('edit-todo-item-title')
+    const editTaskPanel = document.getElementById('edit-item');
+    const NewPanel = document.getElementById('new-item')
+
+
+        // Fonction pour ajouter un item
+    addButton.addEventListener('click', function() {
+        const newItemText = inputField.value.trim();
 
         if (newItemText) {
-            // Créer un nouvel élément <li>
-            const newItem = document.createElement('li');
-            newItem.textContent = newItemText;  // Ajoute le texte de l'item à cet élément <li>
 
-            // Ajouter ce nouvel <li> dans la liste <ol>
-            document.getElementById('todo-list').appendChild(newItem);
+            const newItem = document.createElement('li');
+            const deleteButton = document.createElement('button');
+            const editButton = document.createElement('button')
+
+            deleteButton.textContent = 'Supprimer'
+            editButton.textContent = 'Editer'
+            newItem.textContent = newItemText
+
+            newItem.append(deleteButton)
+            newItem.append(editButton)
+            todoList.appendChild(newItem)
+
+            deleteButton.addEventListener('click',function () {
+                newItem.remove() // Supprimer l'élément de la liste
+            });
+
+            editButton.addEventListener('click',function () {
+                editTaskPanel.hidden = false
+                NewPanel.hidden = true
+
+                confirmEditButton.addEventListener('click',function () {
+                    const newItemText = inputEdit.value.trim();
+                })
+
+                cancelEditButton.addEventListener('click', function () {
+                    editTaskPanel.hidden = true
+                    NewPanel.hidden = false
+                })
+            });
 
             // Réinitialiser l'input pour que l'utilisateur puisse ajouter un autre item
-            document.getElementById('new-todo-item-title').value = '';
+            inputField.value = ''
         }
     });
+});
+
